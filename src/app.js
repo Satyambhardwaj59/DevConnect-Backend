6226614b1d94008335a3d137bc48df62559a59d9
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-05-02 21:46:05 +0530

    Config Express and nodemon for server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("Hello Sam");
+});
+
+app.use("/hello", (req, res) => {
+    res.send("Hello Satyam from the server!");
+});
+
+app.use("/test", (req, res) => {
+    res.send("Hello Tester from the server!");
+});
+
+app.listen(7777, () => {
+    console.log("surver listening.....");
+    
+});
