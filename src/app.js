40b0058a770eece97806a7097fe47397065de377
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-05-03 13:48:05 +0530

    Playing with Advance route with the help of regex but due to express 5 facing problem

--- src/app.js
+++ src/app.js
@@ -2,49 +2,51 @@ const express = require("express");
 
 const app = express();
 
-// app.use('/hello/xyz', (req, res) => {
-//     res.send("Hello xyz!! from the server !")
-// });
-
-// app.use('/hello', (req, res) => {
-//     res.send("Hello from the server !")
-// });
-
-// app.use('/test', (req, res) => {
-//     res.send("Tester getting Hello from the server !")
-// });
-
-// app.use('/', (req, res) => {
-//     res.send("Request from the server !")
-// });
-
-
-
-
 // app.use('/user', (req, res) => {               // ****  if we execute this code then the below code will not work because we are using the same path (use handle all methode ) for both the code. So we need to use different path for both the code.
 //     res.send("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀!")
 // });
 
-app.get('/user', (req, res) => {
-    res.send("Geting request from the server !")
+// app.get('/user', (req, res) => {
+//     res.send("Geting request from the server !")
+// });
+
+// app.get('/user', (req, res) => {
+//     res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
+// });
+
+// app.post('/user', (req, res) => {
+//     res.send("Posting Request from the server !")
+// });
+
+// app.delete('/user', (req, res) => {
+//     res.send("Deleting Request from the server !")
+// });
+
+// app.put('/user', (req, res) => {
+//     res.send("Puting Request from the server !")
+// });
+
+// app.patch('/user', (req, res) => {
+//     res.send("Patching Request from the server !")
+// });
+
+
+//  ********  Advance Routing  ********* //
+
+app.get("/a{b}c", async (req, res) => {
+    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
 });
 
-app.post('/user', (req, res) => {
-    res.send("Posting Request from the server !")
+app.post("/ab{*cd}", async (req, res) => {
+    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
+});
+app.put("/a{bc}d", async (req, res) => {
+    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
 });
 
-app.delete('/user', (req, res) => {
-    res.send("Deleting Request from the server !")
+app.patch("/ab\c", async (req, res) => {
+    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
 });
-
-app.put('/user', (req, res) => {
-    res.send("Puting Request from the server !")
-});
-
-app.patch('/user', (req, res) => {
-    res.send("Patching Request from the server !")
-});
-
 
 
 app.listen(3000, () => {
