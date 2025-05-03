82c7b8ab283fc1af9f00ac11f0d9bbb738a8a2e7
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-05-03 21:53:51 +0530

    Playin with multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,54 +1,102 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-// app.use('/user', (req, res) => {               // ****  if we execute this code then the below code will not work because we are using the same path (use handle all methode ) for both the code. So we need to use different path for both the code.
-//     res.send("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀!")
-// });
+/*
 
-// app.get('/user', (req, res) => {
-//     res.send("Geting request from the server !")
-// });
+It will hang because we didnot send any response to the client.
 
-// app.get('/user', (req, res) => {
-//     res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
-// });
+app.get("/user", (req, res, next) => {
+    console.log("1st User route accessed");
+    // res.send("1st responce accessed");
 
-// app.post('/user', (req, res) => {
-//     res.send("Posting Request from the server !")
-// });
+}, (req, res, next) => {
+    console.log("2nd User route accessed");
+    // res.send("2nd responce accessed");
+});
+*/
 
-// app.delete('/user', (req, res) => {
-//     res.send("Deleting Request from the server !")
-// });
+/*
 
-// app.put('/user', (req, res) => {
-//     res.send("Puting Request from the server !")
-// });
+In console- 1st User route accessed
+In browser- 1st responce accessed
 
-// app.patch('/user', (req, res) => {
-//     res.send("Patching Request from the server !")
-// });
+app.get("/user", (req, res, next) => {
+    console.log("1st User route accessed");
+    res.send("1st responce accessed");
 
-
-//  ********  Advance Routing  ********* //
-
-app.get("/a{b}c", async (req, res) => {
-    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
+}, (req, res, next) => {
+    console.log("2nd User route accessed");
+    res.send("2nd responce accessed");
 });
 
-app.post("/ab{*cd}", async (req, res) => {
-    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
-});
-app.put("/a{bc}d", async (req, res) => {
-    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
-});
+*/
 
-app.patch("/ab\c", async (req, res) => {
-    res.send({firstName: 'Satyam', lastName: 'Bhardwaj'});
+/*
+
+in console- 1st User route accessed
+          - 2nd User route accessed
+In browser- 1st responce accessed
+and also throws error in console- Error: Can't set headers after they are sent. because we are sending two responses to the same request.
+
+app.get("/user", (req, res, next) => {
+    console.log("1st User route accessed");
+
+    res.send("1st responce accessed");
+    next();
+
+}, (req, res, next) => {
+    console.log("2nd User route accessed");
+    res.send("2nd responce accessed");
 });
+*/
+
+/*
+
+in console- 1st User route accessed
+          - 2nd User route accessed
+In browser- 2st responce accessed
+and also throws error in console- Error: Can't set headers after they are sent. because we are sending two responses to the same request.
+Order metter in code excution it will excute line by line and when it reach to next() it will go to 2nd route handler and send the response from there.
+ 
+
+app.get("/user", (req, res, next) => {
+    console.log("1st User route accessed");
+    next();
+    res.send("1st responce accessed");
+    
+
+}, (req, res, next) => {
+    console.log("2nd User route accessed");
+    res.send("2nd responce accessed");
+});
+*/
+
+// we can have multiple route handlers for a single route.
+// we can also wrap the route handlers in an array
+
+app.get("/user", (req, res, next) => 
+
+{
+    console.log("1st User route accessed");
+    next();
+    res.send("1st responce accessed");
+    
+
+}, [(req, res, next) => {
+    console.log("2nd User route accessed");
+    res.send("2nd responce accessed");
+
+}, (req, res, next) => {
+    console.log("3rd User route accessed");
+    res.send("3rd responce accessed");
+
+}, (req, res, next) => {
+    console.log("4th User route accessed");
+    res.send("4th responce accessed");
+}]
+);
 
 
-app.listen(3000, () => {
-    console.log("surver listening..... on 3000");
+app.listen(7777, () => {
+    console.log("Server is running on port 7777");
 });
