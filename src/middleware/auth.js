060a623d34dfa1f7673219fdf13a48b8faee8fbc
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-05-12 00:33:59 +0530

    build userAuth middleware and authenticate in profile and sendConnectionRequest API

---
+++ src/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const  User  = require('../models/user');
+
+const userAuth = async (req, res, next) => {
+    try {
+        const cookie = req.cookies;
+        const {token} = cookie;
+        if(!token ){
+            throw new Error("Token not found");
+        }
+
+        const decodedData = await jwt.verify(token, "DEV@Connect1234");
+        const {_id} = decodedData;
+        const user = await User.findById(_id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        req.user = user;
+        next();
+        
+    } catch (error) {
+        res.status(400).send("ERROR : " + error.message);
+        
+    }
+    
+};
+
+module.exports = {userAuth};
