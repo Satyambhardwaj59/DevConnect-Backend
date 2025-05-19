904097997aa97b14f028bef1f7c5d21d2935b00a
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-05-19 22:08:08 +0530

    Build API /request/review/:status/:requedtId but not working as expected

--- src/routes/request.js
+++ src/routes/request.js
@@ -43,6 +43,36 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth, async (req, res) 
         res.status(500).send("ERROR : " + error.message);
         
     }
-})
+});
+
+requestRouter.post("/request/review/:status/:requedtId", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const {status, requestId} = req.params;
+
+        const allowedStatus = ["accepted", "rejected"];
+        if(!allowedStatus.includes(status)){
+            return res.status(400).send("Invalid status");
+        };
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser.Id,
+            status: "interested",
+        });
+        
+        if(!connectionRequest){
+            return res.status(404).json({message: "Connection request not found"});
+        }
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+        
+        res.status(200).json({message: `Connection request ${status} successfully`, data});
+        
+    } catch (error) {
+        res.status(500).send("ERROR : " + error.message);
+        
+    }
+});
 
 module.exports = requestRouter;
