dc63a1f49cd1336457e181fb2e2068a09cce49d5
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-05-22 21:37:12 +0530

    Build user/connection API to find all connection

--- src/EP-13/part-3/routes/user.js
+++ src/EP-13/part-3/routes/user.js
@@ -5,26 +5,6 @@ const ConnectionRequest = require("../models/connectionRequest");
 
 const USER_SAFE_SHARING_FIELDS = "firstName lastName photoUrl age gender skills about";
 
-userRouter.get("/user/connection/received", userAuth, async (req, res) => {
-    try {
-        const loggedInUser = req.user;
-        const receivedRequsts = await ConnectionRequest.find({
-            toUserId: loggedInUser._id,
-            status: "interested"
-        }).populate("fromUserId", USER_SAFE_SHARING_FIELDS);
-        // }).populate("fromUserId", ["firstName", "lastName", "photoUrl", "age", "gender", "skills", "about"]);
-
-        // if (receivedRequsts.length === 0) {
-        //     return res.status(404).json({ message: "No panding requests found" });
-        // };
-
-        res.status(200).json({ message: "Panding Requests is here", data: receivedRequsts });
-    } catch (error) {
-        res.status(500).send("ERROR : " + error.message);
-        
-    }
-});
-
 userRouter.get("/user/connection", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
@@ -41,7 +21,7 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
         res.status(500).send("ERROR : " + error.message);
         
     }
-})
+});
 
 
 module.exports = userRouter;
