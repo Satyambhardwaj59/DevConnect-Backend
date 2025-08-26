391048debd91ff13e7b029e81366220b1fdaf125
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-08-26 12:37:26 +0530

    allow cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://devconnect-frontend-cbbx.onrender.com", "http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
